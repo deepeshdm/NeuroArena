d32fcfe749d1059586e04d2c79f6c2c07eb0054e
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2026-04-05 15:34:35 +0530

    added room type gri

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-
-
 
    roomTypes = [
     {
