6f8b8c835de6acaf24b4c0772626dcec51bfe816
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2026-04-05 16:20:08 +0530

    hardcore mode design done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'neuroarena-ui';
+
+  openGithub(){
+    window.open('https://github.com/deepeshdm/NeuroArena', '_blank');
+  }
+
 }
