b818fd7100091c9556474821e304d5e177165957
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2026-04-06 22:18:56 +0530

    added lobby comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LobbyComponent } from './lobby/lobby.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LandingPageComponent
+    },
+    {
+        path: 'lobby',
+        component: LobbyComponent
     }
 ];
